f6c59d0d850c485dafa561493ef136944d3cdf29
John Medland <john@controlsoftwaresolutions.com>
2025-03-18 15:56:48 +0000

    Fixed up 2d-array and renamed binary

--- shared-binary/include/g_industrial_cam/lv_interop/lv_array_2d.hpp
+++ shared-binary/include/g_industrial_cam/lv_interop/lv_array_2d.hpp
@@ -14,7 +14,7 @@
 
 #include "./set_packing.hpp"
 
-namespace namespace g_industrial_cam
+namespace g_industrial_cam
 {
     namespace lv_interop
     {
@@ -24,12 +24,6 @@ namespace namespace g_industrial_cam
         {
         public:
             LV_2DArrayHandle_t() = delete;
-
-            cv::Size size() const
-            {
-                auto dims = this->extents();
-                return cv::Size(dims[1], dims[0]);
-            }
         };
     }
 }
