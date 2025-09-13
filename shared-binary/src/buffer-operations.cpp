287d98d0d46a36d0434f579b4cc6d4c57a145e41
John Medland <john@serenial.io>
2025-09-13 20:23:53 +0100

    Working on Drawing to Picture with C++ for the improved speed

--- shared-binary/src/buffer-operations.cpp
+++ shared-binary/src/buffer-operations.cpp
@@ -505,6 +505,119 @@ extern "C"
         return LV_ERR_noError;
     }
 
+    G_INDUSTRIAL_CAM_EXPORT LV_MgErr_t g_industrial_cam_buffer_draw_lv_picture(
+        LV_ErrorClusterPtr_t error_cluster_ptr,
+        LV_EDVRReferencePtr_t edvr_ref_ptr,
+        LV_PictureOffsetPointPtr_t top_left_ptr,
+        LV_StringHandle_t lv_str_handle,
+        LV_PictureOpHeaderPtr_t op_header_ptr,
+        uint8_t pixel_format
+    )
+    {
+        try
+        {
+
+            lv_buffer buffer(edvr_ref_ptr);
+            auto format = static_cast<pixel_formats>(pixel_format);
+            auto is_greyscale = format != pixel_formats::BGR8 && format != pixel_formats::RGB8;
+            
+            int32_t output_width = buffer.width();
+
+            size_t mask_row_length = 0, mask_bytes_length = 0;
+
+            if (is_greyscale && output_width % 2 != 0)
+            {
+                output_width++;
+            }
+
+            size_t image_bytes_length;
+            size_t total_data_length;
+
+            if(is_greyscale)
+            {
+                image_bytes_length = output_width * buffer.height();
+                total_data_length = SIZEOF_LV_FLATTENED_PICTURE_OP_HEADER_BYTES + image_bytes_length + lv_picture::greyscale_lookup_length;
+            }
+            else
+            {
+                image_bytes_length = output_width * buffer.height() * 3;
+                total_data_length = SIZEOF_LV_FLATTENED_PICTURE_OP_HEADER_BYTES + 2 + image_bytes_length; // add sizeof uint16_t to account for unused colour table
+            }
+
+            // size source string-handle to hold our image data
+            lv_str_handle.size_to_fit(total_data_length);
+
+            // use some magic numbers to set the header
+            op_header_ptr->op_code = 29; // draw rgb data
+            op_header_ptr->image_bytes_length = image_bytes_length;
+            op_header_ptr->bitwidth = is_greyscale ? 8 : 24;
+            op_header_ptr->default_background = 0x00FFFFFF; // white
+            op_header_ptr->default_foreground = 0x00000000; // black
+            op_header_ptr->top = top_left_ptr->m_y;
+            op_header_ptr->left = top_left_ptr->m_x;
+            op_header_ptr->bottom = op_header_ptr->top + buffer.height();
+            op_header_ptr->right = op_header_ptr->left + buffer.width();
+
+
+            if(buffer.size() == 0){
+                return LV_ERR_noError;
+            }
+
+            // get a pointer to the point in the "string" bytes where the rgb pixel data starts
+            auto dst_data_ptr = lv_str_handle.begin() + SIZEOF_LV_FLATTENED_PICTURE_OP_HEADER_BYTES;
+
+            if(is_greyscale){
+            //     op_header_ptr->length = image_bytes_length + 22 + lv_picture::greyscale_lookup_length + mask_bytes_length;
+
+            //     // copy the greyscale colour-table into string
+            //     std::memcpy(dst_data_ptr, lv_picture::greyscale_lookup, lv_picture::greyscale_lookup_length);
+
+            //     dst_data_ptr += lv_picture::greyscale_lookup_length;
+
+            //     cv::Mat dst(dst_size, CV_8UC1, dst_data_ptr);
+
+            //     cv::Mat src_sized_dst = dst(cv::Rect(cv::Point(0, 0), src.size()));
+            //     src.copyTo(src_sized_dst);
+
+            }
+            else{
+
+                op_header_ptr->length = image_bytes_length + mask_bytes_length + 24; // 24 = 22 + 2 bytes for the empty mask
+
+                // set the first two bytes to zero which is a uint16_t value representing that the colour table is not used (length 0)
+
+                for (size_t i = 0; i < 2; i++)
+                {
+                    *dst_data_ptr = 0;
+                    dst_data_ptr++;
+                }
+
+                switch (format){
+                    case pixel_formats::RGB8 :
+                        // straight copy
+                        std::memcpy(dst_data_ptr, buffer.begin(), buffer.size());
+                    break;
+                    case pixel_formats::BGR8 :
+                        auto src_data_ptr = buffer.begin();
+                        // copy with pixel byte order swapped
+                        while(src_data_ptr < buffer.end()){
+                            *(dst_data_ptr + 0) = *(src_data_ptr + 2);
+                            *(dst_data_ptr + 1) = *(src_data_ptr + 1);
+                            *(dst_data_ptr + 2) = *(src_data_ptr + 0);
+                            src_data_ptr +=3;
+                            dst_data_ptr +=3;
+                        }
+                    break;
+                }
+            }
+        }
+        catch (...)
+        {
+            error_cluster_ptr.copy_from_exception(std::current_exception(), __func__);
+        }
+        return LV_ERR_noError;
+    }
+
     G_INDUSTRIAL_CAM_EXPORT LV_MgErr_t g_industrial_cam_buffer_get_size_in_bytes(
         LV_ErrorClusterPtr_t error_cluster_ptr,
         LV_EDVRReferencePtr_t edvr_ref_ptr,
