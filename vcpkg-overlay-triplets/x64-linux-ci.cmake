71036d68e5bab3a6ec13e2f0f2f748bb7ca2845b
John Medland <j-medland@users.noreply.github.com>
2025-09-16 22:46:23 +0100

    Update x64-linux-ci.cmake

--- vcpkg-overlay-triplets/x64-linux-ci.cmake
+++ vcpkg-overlay-triplets/x64-linux-ci.cmake
@@ -3,3 +3,6 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -static-libstdc++")
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -static-libgcc")
