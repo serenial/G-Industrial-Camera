15fd22690a5c2e16ce29d3c5cf9f75c7278a0356
John Medland <j-medland@users.noreply.github.com>
2025-09-16 21:52:04 +0100

    Update x64-linux-ci.cmake

--- vcpkg-overlay-triplets/x64-linux-ci.cmake
+++ vcpkg-overlay-triplets/x64-linux-ci.cmake
@@ -1,7 +1,7 @@
 # vcpkg\triplets\community\x64-linux-release.cmake
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_CRT_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
