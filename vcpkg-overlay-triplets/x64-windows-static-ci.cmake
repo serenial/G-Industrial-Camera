66488e71044d52ab9f1e2b632f4a867c4b573ef7
John Medland <john@serenial.io>
2025-09-29 23:52:55 +0100

    tweaking build parameters

--- vcpkg-overlay-triplets/x64-windows-static-ci.cmake
+++ vcpkg-overlay-triplets/x64-windows-static-ci.cmake
@@ -1,8 +1,9 @@
 # vcpkg\triplets\community\x64-windows-static-release.cmake
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_BUILD_TYPE release)
-
 set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+
+set(MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
